647abbf7bf6fe59dc74eb6903a8407e4177ef61d
Deleranax <deleranax@gmail.com>
2023-10-17 22:38:42 +0200

    Adding primary classes
    Adding primary forms of data holding for the simulator.

--- src/main/java/fr/dwightstudio/jarmemu/sim/StateContainer.java
+++ src/main/java/fr/dwightstudio/jarmemu/sim/StateContainer.java
@@ -2,4 +2,35 @@ package fr.dwightstudio.jarmemu.sim;
 
 public class StateContainer {
 
+    // Registers
+    public static final int REGISTER_NUMBER = 16;
+    public final Register[] registers;
+
+    // Memory
+    public static final int MEMORY_CHUNK_NUMBER = 256;
+    public static final int MEMORY_CHUNK_SIZE = 4;
+    public static final int MEMORY_SIZE = MEMORY_CHUNK_NUMBER * MEMORY_CHUNK_SIZE;
+    public final Byte[] memory;
+
+    public StateContainer() {
+
+        // Initializing registers
+        this.registers = new Register[REGISTER_NUMBER];
+        clearRegisters();
+
+        // Initializing memory
+        this.memory = new Byte[MEMORY_SIZE];
+    }
+
+    public void clearRegisters() {
+        for (int i = 0 ; i < REGISTER_NUMBER ; i ++) {
+            registers[i] = new Register();
+        }
+    }
+
+    public void clearMemory() {
+        for (int i = 0 ; i < MEMORY_SIZE ; i ++) {
+            memory[i] = (byte) 0;
+        }
+    }
 }
---
+++ src/test/java/jarmemu/asm/SourceReader.java
@@ -0,0 +1,4 @@
+package jarmemu.asm;
+
+public class SourceReader {
+}
