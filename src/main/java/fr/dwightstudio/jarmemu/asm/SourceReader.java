6caac559bc509b3a17a2082611d26561a487df7f
Kévin Tollemer <kevin.tollemer@insa-rennes.fr>
2023-10-18 09:45:06 +0200

    Base du SourceReader
    Le SourceReader peut maintenant décortiquer les lignes, mais il ne fait rien de plus (pas d'exécutions).

--- src/main/java/fr/dwightstudio/jarmemu/asm/SourceReader.java
+++ src/main/java/fr/dwightstudio/jarmemu/asm/SourceReader.java
@@ -5,11 +5,21 @@ import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class SourceReader {
 
     private final Scanner scanner;
+    private final Logger logger = Logger.getLogger(getClass().getName());
+    private Instruction instruction;
+    private boolean updateFlags;
+    private boolean isHalfWord;
+    private boolean isByte;
+    private UpdateMode updateMode;
+    private Condition conditionExec;
 
     /**
      * Création du lecteur du fichier *.s
@@ -19,6 +29,12 @@ public class SourceReader {
     public SourceReader(URI fileName) throws FileNotFoundException {
         File file = new File(fileName);
         this.scanner = new Scanner(file);
+        this.instruction = null;
+        this.updateFlags = false;
+        this.isHalfWord = false;
+        this.isByte = false;
+        this.updateMode = null;
+        this.conditionExec = null;
     }
 
     /**
@@ -40,15 +56,73 @@ public class SourceReader {
     }
 
     /**
+     * Retire les drapeaux S, H, B et les "update modes"
+     * @param instructionString La ligne à modifier
+     * @return La ligne modifiée ou non
+     */
+    public String removeFlags(String instructionString){
+        if (instructionString.endsWith("S") && (instructionString.length()%2==0)){
+            updateFlags = true;
+            instructionString = instructionString.substring(0, instructionString.length()-1);
+        } else if (instructionString.endsWith("H")) {
+            isHalfWord = true;
+            instructionString = instructionString.substring(0, instructionString.length()-1);
+        } else if (instructionString.endsWith("B")) {
+            isByte = true;
+            instructionString = instructionString.substring(0, instructionString.length()-1);
+        } else if (instructionString.length()==7 || instructionString.length()==5) {
+            UpdateMode[] updateModes = UpdateMode.values();
+            for (UpdateMode updatemode:updateModes) {
+                if (instructionString.endsWith(updatemode.toString().toUpperCase())) updateMode = updatemode;
+            }
+        }
+        return instructionString;
+    }
+
+    /**
+     * Retire les conditions d'exécution
+     * @param instructionString La ligne à modifier
+     * @return La ligne modifiée ou non
+     */
+    public String removeCondition(String instructionString){
+        Condition[] conditions = Condition.values();
+        for (Condition condition:conditions) {
+            if (instructionString.endsWith(condition.toString().toUpperCase())){
+                if (instructionString.endsWith("MLAL")) continue;
+                if (instructionString.endsWith("TEQ")) continue;
+
+                conditionExec = condition;
+                instructionString = instructionString.substring(instructionString.length()-2);
+            }
+        }
+        return instructionString;
+    }
+
+    /**
      * Méthode principale
      * Lecture du fichier et envoie des instructions
      */
     public void read(){
         String line;
+        String instructionString;
+
         while (this.scanner.hasNextLine()){
             line = this.scanner.nextLine();
             line = this.removeComments(line);
             line = this.removeBlanks(line);
+            line = line.toUpperCase();
+
+            instructionString = line.split(" ")[0];
+            instructionString = this.removeFlags(instructionString);
+            instructionString = this.removeCondition(instructionString);
+
+            Instruction[] instructions = Instruction.values();
+            for (Instruction instruction:instructions) {
+                if(instruction.toString().toUpperCase().equals(instructionString)) this.instruction = instruction;
+            }
+
+            if (this.instruction == null) throw new IllegalStateException();
+
         }
     }
 
