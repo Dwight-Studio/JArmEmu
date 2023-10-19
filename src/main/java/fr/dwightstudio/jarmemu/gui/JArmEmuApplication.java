9a96b162aef954e84ed8c649c2bafd419bcfa07b
Alexandre Leconte <alexandre.leconte@insa-rennes.fr>
2023-10-19 23:28:34 +0200

    Ajout de la configuration JavaFX

--- src/main/java/fr/dwightstudio/jarmemu/gui/JArmEmuApplication.java
+++ src/main/java/fr/dwightstudio/jarmemu/gui/JArmEmuApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -12,6 +13,7 @@ public class JArmEmuApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(JArmEmuApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
