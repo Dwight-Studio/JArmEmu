1dd9fb2dd7f6bb680a3b388b668a5820d87449d5
Alexandre Leconte <aleconte@insa-rennes.fr>
2023-10-23 17:40:25 +0200

    Ajout de MathUtils
    Ajout de Carry

--- src/main/java/fr/dwightstudio/jarmemu/util/MathUtils.java
+++ src/main/java/fr/dwightstudio/jarmemu/util/MathUtils.java
@@ -2,10 +2,16 @@ package fr.dwightstudio.jarmemu.util;
 
 public class MathUtils {
 
-    public static boolean isOverflow(int a, int b) {
+    public static boolean hasOverflow(int a, int b) {
         int r = a + b;
         // Overflow if both arguments have the opposite sign of the result
         return ((a ^ r) & (b ^ r)) < 0;
+    }
+
+    public static boolean hasCarry(int a, int b) {
+        int r = a + b;
+        // Overflow if both arguments have the opposite sign of the result
+        return a > 0 && b > 0 && r < 0;
     }
 
 }
