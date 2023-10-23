9b17efed58ba53818450f9de503b9729937e29b1
joshuadwightred <tollemerkevin@gmail.com>
2023-10-23 23:06:58 +0200

    Minor update
    -Changed the order of two methods for my sanity

--- src/main/java/fr/dwightstudio/jarmemu/util/MathUtils.java
+++ src/main/java/fr/dwightstudio/jarmemu/util/MathUtils.java
@@ -2,17 +2,16 @@ package fr.dwightstudio.jarmemu.util;
 
 public class MathUtils {
 
-    public static boolean hasOverflow(int a, int b) {
-        int r = a + b;
-        // Overflow if both arguments have the opposite sign of the result
-        return ((a ^ r) & (b ^ r)) < 0;
-    }
-
     public static boolean hasCarry(int a, int b) {
         int r = a + b;
         // Overflow if both arguments have the opposite sign of the result and are positive
         return a > 0 && b > 0 && r < 0;
     }
 
+    public static boolean hasOverflow(int a, int b) {
+        int r = a + b;
+        // Overflow if both arguments have the opposite sign of the result
+        return ((a ^ r) & (b ^ r)) < 0;
+    }
 
 }
--- src/test/java/fr/dwightstudio/jarmemu/asm/inst/ADDExecutorTest.java
+++ src/test/java/fr/dwightstudio/jarmemu/asm/inst/ADDExecutorTest.java
@@ -85,14 +85,14 @@ public class ADDExecutorTest {
         addExecutor.execute(stateContainer, true, null, null, r2, r1, r0.getData(), ArgumentParsers.SHIFT.none());
         assertTrue(stateContainer.cpsr.getN());
         assertFalse(stateContainer.cpsr.getZ());
-        assertFalse(stateContainer.cpsr.getC());
+        assertFalse(stateContainer.cpsr.getC()); //Renvoie true au lieu de false
         assertTrue(stateContainer.cpsr.getV());
         r0.setData(0b11111111111111111111111111111111);
         r1.setData(1);
         addExecutor.execute(stateContainer, true, null, null, r2, r1, r0.getData(), ArgumentParsers.SHIFT.none());
         assertFalse(stateContainer.cpsr.getN());
         assertTrue(stateContainer.cpsr.getZ());
-        assertTrue(stateContainer.cpsr.getC());
+        assertTrue(stateContainer.cpsr.getC()); //Renvoie false au lieu de true
         assertFalse(stateContainer.cpsr.getV());
     }
 
