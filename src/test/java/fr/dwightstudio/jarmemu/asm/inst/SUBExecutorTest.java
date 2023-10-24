7ef7cc48a37339314a016c90b3389b37676ee86d
joshuadwightred <tollemerkevin@gmail.com>
2023-10-24 13:56:14 +0200

    Minor update
    -SUBExecutor is done and so are its tests

--- src/test/java/fr/dwightstudio/jarmemu/asm/inst/SUBExecutorTest.java
+++ src/test/java/fr/dwightstudio/jarmemu/asm/inst/SUBExecutorTest.java
@@ -46,10 +46,27 @@ public class SUBExecutorTest {
         Register r2 = stateContainer.registers[2];
         r0.setData(0b10000000000000000000000000000000);
         r1.setData(1);
-        subExecutor.execute(stateContainerBis, true, null, null, r2, r0, r1.getData(), ArgumentParsers.SHIFT.none());
+        subExecutor.execute(stateContainer, true, null, null, r2, r0, r1.getData(), ArgumentParsers.SHIFT.none());
+        assertEquals(0b01111111111111111111111111111111, r2.getData());
         assertFalse(stateContainer.cpsr.getN());
         assertFalse(stateContainer.cpsr.getZ());
         assertTrue(stateContainer.cpsr.getC());
+        assertTrue(stateContainer.cpsr.getV());
+        r0.setData(0b11111111111111111111111111111111);
+        r1.setData(0b11111111111111111111111111111111);
+        subExecutor.execute(stateContainer, true, null, null, r2, r0, r1.getData(), ArgumentParsers.SHIFT.none());
+        assertEquals(0, r2.getData());
+        assertFalse(stateContainer.cpsr.getN());
+        assertTrue(stateContainer.cpsr.getZ());
+        assertTrue(stateContainer.cpsr.getC());
+        assertFalse(stateContainer.cpsr.getV());
+        r0.setData(0b01111111111111111111111111111111);
+        r1.setData(0b11111111111111111111111111111111);
+        subExecutor.execute(stateContainer, true, null, null, r2, r0, r1.getData(), ArgumentParsers.SHIFT.none());
+        assertEquals(0b10000000000000000000000000000000, r2.getData());
+        assertTrue(stateContainer.cpsr.getN());
+        assertFalse(stateContainer.cpsr.getZ());
+        assertFalse(stateContainer.cpsr.getC());
         assertTrue(stateContainer.cpsr.getV());
     }
 
