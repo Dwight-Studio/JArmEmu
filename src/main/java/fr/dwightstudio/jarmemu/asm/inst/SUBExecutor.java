2f346f9d4400707a486c1113ce2bd761c504a44a
joshuadwightred <tollemerkevin@gmail.com>
2023-10-24 13:57:11 +0200

    Minor update
    -SUBExecutor is done and so are its tests

--- src/main/java/fr/dwightstudio/jarmemu/asm/inst/SUBExecutor.java
+++ src/main/java/fr/dwightstudio/jarmemu/asm/inst/SUBExecutor.java
@@ -10,7 +10,6 @@ import fr.dwightstudio.jarmemu.util.MathUtils;
 public class SUBExecutor implements InstructionExecutor<Register, Register, Integer, ShiftParser.ShiftFunction> {
     @Override
     public void execute(StateContainer stateContainer, boolean updateFlags, DataMode dataMode, UpdateMode updateMode, Register arg1, Register arg2, Integer arg3, ShiftParser.ShiftFunction arg4) {
-        //TODO: Faire l'instruction SUB
         int i1 = arg4.apply(arg3);
 
         arg1.setData(arg2.getData() - i1); // arg1 = arg2 - (arg4 SHIFT arg3)
