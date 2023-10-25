0f3a886d7ddf86da038a691add49a9e8aadc1e4e
joshuadwightred <tollemerkevin@gmail.com>
2023-10-25 21:36:52 +0200

    Minor update
    -Removed TODO

--- src/main/java/fr/dwightstudio/jarmemu/asm/inst/ANDExecutor.java
+++ src/main/java/fr/dwightstudio/jarmemu/asm/inst/ANDExecutor.java
@@ -9,7 +9,6 @@ import fr.dwightstudio.jarmemu.sim.StateContainer;
 public class ANDExecutor implements InstructionExecutor<Register, Register, Integer, ShiftParser.ShiftFunction> {
     @Override
     public void execute(StateContainer stateContainer, boolean updateFlags, DataMode dataMode, UpdateMode updateMode, Register arg1, Register arg2, Integer arg3, ShiftParser.ShiftFunction arg4) {
-        //TODO: Faire l'instruction AND
         int i1 = arg4.apply(arg3);
 
         arg1.setData(arg2.getData() & i1);
