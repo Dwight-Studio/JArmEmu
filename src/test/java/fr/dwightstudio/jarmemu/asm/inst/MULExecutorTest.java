0fb3ab22ec94c4b65724e6cef786c99503791ea4
joshuadwightred <tollemerkevin@gmail.com>
2023-10-30 19:26:36 +0100

    Minor update
    -MUL instruction tests done

--- src/test/java/fr/dwightstudio/jarmemu/asm/inst/MULExecutorTest.java
+++ src/test/java/fr/dwightstudio/jarmemu/asm/inst/MULExecutorTest.java
@@ -1,8 +1,12 @@
 package fr.dwightstudio.jarmemu.asm.inst;
 
+import fr.dwightstudio.jarmemu.asm.args.ArgumentParsers;
+import fr.dwightstudio.jarmemu.sim.obj.Register;
 import fr.dwightstudio.jarmemu.sim.obj.StateContainer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MULExecutorTest {
 
@@ -16,8 +20,40 @@ public class MULExecutorTest {
     }
 
     @Test
-    public void simpleBTest() {
-        //TODO: Faire les tests de l'instruction MUL
+    public void simpleMulTest() {
+        Register r0 = stateContainer.registers[0];
+        Register r1 = stateContainer.registers[1];
+        Register r2 = stateContainer.registers[2];
+        r0.setData(16);
+        r1.setData(16);
+        mulExecutor.execute(stateContainer, false, null, null, r2, r1, r0, ArgumentParsers.SHIFT.none());
+        assertEquals(16*16, r2.getData());
+        r1.setData(-1);
+        mulExecutor.execute(stateContainer, false, null, null, r0, r1, r2, ArgumentParsers.SHIFT.none());
+        assertEquals(-16*16, r0.getData());
+        r0.setData(8594297);
+        r1.setData(859425);
+        mulExecutor.execute(stateContainer, false, null, null, r2, r1, r0, ArgumentParsers.SHIFT.none());
+        assertEquals(-1190049895, r2.getData());
+    }
+
+    @Test
+    public void flagsTest() {
+        Register r0 = stateContainer.registers[0];
+        Register r1 = stateContainer.registers[1];
+        r0.setData(16);
+        r1.setData(16);
+        mulExecutor.execute(stateContainer, true, null, null, r1, r1, r0, ArgumentParsers.SHIFT.none());
+        assertFalse(stateContainer.cpsr.getN());
+        assertFalse(stateContainer.cpsr.getZ());
+        r1.setData(-1);
+        mulExecutor.execute(stateContainer, true, null, null, r1, r1, r0, ArgumentParsers.SHIFT.none());
+        assertTrue(stateContainer.cpsr.getN());
+        assertFalse(stateContainer.cpsr.getZ());
+        r1.setData(0);
+        mulExecutor.execute(stateContainer, true, null, null, r1, r1, r0, ArgumentParsers.SHIFT.none());
+        assertFalse(stateContainer.cpsr.getN());
+        assertTrue(stateContainer.cpsr.getZ());
     }
 
 }
