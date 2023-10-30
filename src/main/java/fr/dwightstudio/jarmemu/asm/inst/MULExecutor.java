fd066a090f8401e3131ab6bafd9f3ab67b463925
joshuadwightred <tollemerkevin@gmail.com>
2023-10-30 23:57:27 +0100

    Minor update
    -Removed TODO

--- src/main/java/fr/dwightstudio/jarmemu/asm/inst/MULExecutor.java
+++ src/main/java/fr/dwightstudio/jarmemu/asm/inst/MULExecutor.java
@@ -9,7 +9,6 @@ import fr.dwightstudio.jarmemu.util.MathUtils;
 public class MULExecutor implements InstructionExecutor<Register, Register, Register, Object> {
     @Override
     public void execute(StateContainer stateContainer, boolean updateFlags, DataMode dataMode, UpdateMode updateMode, Register arg1, Register arg2, Register arg3, Object arg4) {
-        //TODO: Faire l'instruction MUL
         arg1.setData(arg2.getData() * arg3.getData()); // arg1 = arg2 * arg3
 
         if (updateFlags) {
