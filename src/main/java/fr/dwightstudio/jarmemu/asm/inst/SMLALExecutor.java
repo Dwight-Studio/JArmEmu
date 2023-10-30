33842d01abc40b208ed9ffc7dbe553a85ff37094
joshuadwightred <tollemerkevin@gmail.com>
2023-10-30 23:56:45 +0100

    Minor update
    -Instruction SMLAL fixed and tests done

--- src/main/java/fr/dwightstudio/jarmemu/asm/inst/SMLALExecutor.java
+++ src/main/java/fr/dwightstudio/jarmemu/asm/inst/SMLALExecutor.java
@@ -8,10 +8,9 @@ import fr.dwightstudio.jarmemu.sim.obj.StateContainer;
 public class SMLALExecutor implements InstructionExecutor<Register, Register, Register, Register> {
     @Override
     public void execute(StateContainer stateContainer, boolean updateFlags, DataMode dataMode, UpdateMode updateMode, Register arg1, Register arg2, Register arg3, Register arg4) {
-        //TODO: Faire FONCTIONNER l'instruction SMLAL
         long r3 = arg3.getData();
         long r4 = arg4.getData();
-        long result = r3 * r4 + arg1.getData() + ((long) arg2.getData() << 32);
+        long result = (((long) arg2.getData() << 32) | (arg1.getData() & 0xFFFFFFFFL)) + r3 * r4;
         arg1.setData((int) (result));
         arg2.setData((int) (result >> 32));
 
