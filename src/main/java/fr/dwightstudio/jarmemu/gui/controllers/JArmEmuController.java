9d70e90f504d2f8965c624753af463667b89e93a
Alexandre Leconte <alexandre.leconte@insa-rennes.fr>
2023-11-02 14:53:14 +0100

    Correction de bugs due à la restructuration

--- src/main/java/fr/dwightstudio/jarmemu/gui/controllers/JArmEmuController.java
+++ src/main/java/fr/dwightstudio/jarmemu/gui/controllers/JArmEmuController.java
@@ -1,38 +1,20 @@
 package fr.dwightstudio.jarmemu.gui.controllers;
 
 import fr.dwightstudio.jarmemu.JArmEmuApplication;
-import fr.dwightstudio.jarmemu.Status;
-import fr.dwightstudio.jarmemu.asm.args.ArgumentParsers;
-import fr.dwightstudio.jarmemu.gui.LineStatus;
-import fr.dwightstudio.jarmemu.sim.SourceScanner;
-import fr.dwightstudio.jarmemu.sim.obj.AssemblyError;
-import fr.dwightstudio.jarmemu.sim.obj.StateContainer;
-import fr.dwightstudio.jarmemu.util.MathUtils;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
-import org.controlsfx.dialog.ExceptionDialog;
 import org.fxmisc.richtext.CodeArea;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
-public class JArmEmuController implements Initializable {
-
-    private final JArmEmuApplication application;
+public class JArmEmuController extends AbstractJArmEmuModule {
 
     private final Logger logger = Logger.getLogger(getClass().getName());
 
@@ -79,96 +61,96 @@ public class JArmEmuController implements Initializable {
     protected ArrayList<Text> stack;
 
     public JArmEmuController(JArmEmuApplication application) {
-        this.application = application;
+        super(application);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        application.getEditorController().initialize(url, resourceBundle);
-        application.getMainMenuController().initialize(url, resourceBundle);
-        application.getMemoryController().initialize(url, resourceBundle);
-        application.getRegistersController().initialize(url, resourceBundle);
-        application.getSettingsController().initialize(url, resourceBundle);
-        application.getSimulationMenuController().initialize(url, resourceBundle);
-        application.getStackController().initialize(url, resourceBundle);
+        getEditorController().initialize(url, resourceBundle);
+        getMainMenuController().initialize(url, resourceBundle);
+        getMemoryController().initialize(url, resourceBundle);
+        getRegistersController().initialize(url, resourceBundle);
+        getSettingsController().initialize(url, resourceBundle);
+        getSimulationMenuController().initialize(url, resourceBundle);
+        getStackController().initialize(url, resourceBundle);
 
         stack = new ArrayList<>();
 
-        onNewFile();
+        getMainMenuController().onNewFile();
 
-        application.getExecutionWorker().revive();
-        application.getExecutionWorker().updateGUI();
+        getExecutionWorker().revive();
+        getExecutionWorker().updateGUI();
     }
 
     @FXML
     protected void onNewFile() {
-        application.getMainMenuController().onNewFile();
+        getMainMenuController().onNewFile();
     }
 
     @FXML
     protected void onOpen() {
-        application.getMainMenuController().onOpen();
+        getMainMenuController().onOpen();
     }
 
     @FXML
     protected void onSave() {
-        application.getMainMenuController().onSave();
+        getMainMenuController().onSave();
     }
 
     @FXML
     protected void onSaveAs() {
-        application.getMainMenuController().onSaveAs();
+        getMainMenuController().onSaveAs();
     }
 
     @FXML
     protected void onReload() {
-        application.getMainMenuController().onReload();
+        getMainMenuController().onReload();
     }
 
     @FXML
     protected void onExit() {
-        application.getMainMenuController().onExit();
+        getMainMenuController().onExit();
     }
 
     @FXML
     protected void onSimulate() {
-        application.getSimulationMenuController().onSimulate();
+        getSimulationMenuController().onSimulate();
     }
 
     @FXML
     protected void onStepInto() {
-        application.getSimulationMenuController().onStepInto();
+        getSimulationMenuController().onStepInto();
     }
 
     @FXML
     protected void onStepOver() {
-        application.getSimulationMenuController().onStepOver();
+        getSimulationMenuController().onStepOver();
     }
 
     @FXML
     protected void onContinue() {
-        application.getSimulationMenuController().onContinue();
+        getSimulationMenuController().onContinue();
     }
 
     @FXML
     protected void onPause() {
-        application.getSimulationMenuController().onPause();
+        getSimulationMenuController().onPause();
     }
 
     @FXML
     protected void onStop() {
-        application.getSimulationMenuController().onStop();
+        getSimulationMenuController().onStop();
     }
 
     @FXML
     protected void onRestart() {
-        application.getSimulationMenuController().onRestart();
+        getSimulationMenuController().onRestart();
     }
 
     @FXML
     protected void onReset() {
-        application.getSimulationMenuController().onReset();
+        getSimulationMenuController().onReset();
     }
 
     @FXML
@@ -179,5 +161,10 @@ public class JArmEmuController implements Initializable {
     @FXML
     protected void onLegacyParser() {
 
+    }
+
+    @FXML
+    protected void onClearNotifs() {
+        getEditorController().clearNotifs();
     }
 }
