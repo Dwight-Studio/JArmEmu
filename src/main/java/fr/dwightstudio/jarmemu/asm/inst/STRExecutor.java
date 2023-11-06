55bf3535e5c3de0771c6c00b5559e610c2e0a4b7
Kévin Tollemer <kevin.tollemer@insa-rennes.fr>
2023-11-06 13:42:48 +0100

    Minor update
    -Removed finished TODOs

--- src/main/java/fr/dwightstudio/jarmemu/asm/inst/STRExecutor.java
+++ src/main/java/fr/dwightstudio/jarmemu/asm/inst/STRExecutor.java
@@ -10,7 +10,6 @@ import fr.dwightstudio.jarmemu.sim.obj.StateContainer;
 public class STRExecutor implements InstructionExecutor<Register, AddressParser.UpdatableInteger, Integer, ShiftParser.ShiftFunction> {
     @Override
     public void execute(StateContainer stateContainer, boolean updateFlags, DataMode dataMode, UpdateMode updateMode, Register arg1, AddressParser.UpdatableInteger arg2, Integer arg3, ShiftParser.ShiftFunction arg4) {
-        //TODO: Faire l'instruction STR
         int i1 = arg4.apply(arg3);
 
         switch (dataMode){
