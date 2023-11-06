e4cff265e7e126b85210e69cf57ca0cf16262208
Kévin Tollemer <kevin.tollemer@insa-rennes.fr>
2023-11-06 13:39:21 +0100

    Minor update
    -Added LDR instruction without its tests

--- src/main/java/fr/dwightstudio/jarmemu/asm/inst/LDRExecutor.java
+++ src/main/java/fr/dwightstudio/jarmemu/asm/inst/LDRExecutor.java
@@ -11,6 +11,14 @@ public class LDRExecutor implements InstructionExecutor<Register, AddressParser.
     @Override
     public void execute(StateContainer stateContainer, boolean updateFlags, DataMode dataMode, UpdateMode updateMode, Register arg1, AddressParser.UpdatableInteger arg2, Integer arg3, ShiftParser.ShiftFunction arg4) {
         //TODO: Faire l'instruction LDR
-        throw new IllegalStateException("Instruction LDR not implemented");
+        int i1 = arg4.apply(arg3);
+
+        switch (dataMode){
+            case null -> arg1.setData(stateContainer.memory.getWord(arg2.toInt() + i1));
+            case HALF_WORD -> arg1.setData(stateContainer.memory.getHalf(arg2.toInt() + i1));
+            case BYTE -> arg1.setData(stateContainer.memory.getByte(arg2.toInt() + i1));
+        }
+
+        arg2.update();
     }
 }
