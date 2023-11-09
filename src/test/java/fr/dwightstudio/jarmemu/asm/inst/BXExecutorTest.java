969fcbb706deeec375907a5a045dfb3f606d504b
joshuadwightred <tollemerkevin@gmail.com>
2023-11-09 19:41:40 +0100

    Minor update
    -Fixed BX instruction
    -Removed TODO
    -Test for BX instruction

--- src/test/java/fr/dwightstudio/jarmemu/asm/inst/BXExecutorTest.java
+++ src/test/java/fr/dwightstudio/jarmemu/asm/inst/BXExecutorTest.java
@@ -1,9 +1,13 @@
 package fr.dwightstudio.jarmemu.asm.inst;
 
 import fr.dwightstudio.jarmemu.JArmEmuTest;
+import fr.dwightstudio.jarmemu.sim.args.LabelParser;
+import fr.dwightstudio.jarmemu.sim.obj.Register;
 import fr.dwightstudio.jarmemu.sim.obj.StateContainer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BXExecutorTest extends JArmEmuTest {
 
@@ -18,7 +22,17 @@ public class BXExecutorTest extends JArmEmuTest {
 
     @Test
     public void simpleBxTest() {
-        //TODO: Faire les tests de BX
+        Register lr = stateContainer.registers[0];
+        Register pc = stateContainer.registers[15];
+        lr.setData(24);
+        pc.setData(48);
+        bxExecutor.execute(stateContainer, false, null, null, lr, null, null, null);
+        assertEquals(24, pc.getData());
+        assertFalse(stateContainer.cpsr.getT());
+        lr.setData(45);
+        bxExecutor.execute(stateContainer, false, null, null, lr, null, null, null);
+        assertEquals(45, pc.getData());
+        assertTrue(stateContainer.cpsr.getT());
     }
 
 }
