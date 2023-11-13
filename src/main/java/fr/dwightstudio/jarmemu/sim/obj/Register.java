46f00930a278c24ac07f8156b3fe69970992b5be
Alexandre Leconte <alexandre.leconte@insa-rennes.fr>
2023-11-13 22:03:26 +0100

    Changement d'organisation des Registers

--- src/main/java/fr/dwightstudio/jarmemu/sim/obj/Register.java
+++ src/main/java/fr/dwightstudio/jarmemu/sim/obj/Register.java
@@ -8,7 +8,7 @@ public class Register {
     private final IntegerProperty dataProperty;
 
     public Register() {
-        this.dataProperty = new SimpleIntegerProperty();
+        this.dataProperty = new SimpleIntegerProperty(0);
     }
 
     public int getData() {
@@ -22,28 +22,34 @@ public class Register {
     public boolean get(int index) throws IllegalArgumentException {
         if (index >= 32) throw new IllegalArgumentException("Invalid index: " + index);
 
-        return ((dataProperty.get() >> index) & 1) == 1;
+        int data = dataProperty.get();
+
+        return ((data >> index) & 1) == 1;
     }
 
     public void set(int index, boolean value) {
         if (index >= 32) throw new IllegalArgumentException("Invalid index: " + index);
 
+        int data = dataProperty.get();
+
         if (value) {
-            dataProperty.set(dataProperty.get() | (1 << index)); // set a bit to 1
+            data |= (1 << index); // set a bit to 1
         } else {
-            dataProperty.set(dataProperty.get() & ~(1 << index)); // set a bit to 0
+            data &= ~(1 << index); // set a bit to 0
         }
+
+        dataProperty.set(data);
     }
 
     public void add(int value) {
-        this.dataProperty.add(value);
+        dataProperty.set(dataProperty.get() + value);
     }
 
     public boolean isPSR() {
         return false;
     }
 
-    public IntegerProperty getProperty() {
+    public IntegerProperty getDataProperty() {
         return dataProperty;
     }
 }
