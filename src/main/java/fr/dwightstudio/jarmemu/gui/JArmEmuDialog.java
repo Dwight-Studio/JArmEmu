0bd5f5662228e7a743a05f83d2023e4953b21225
Alexandre Leconte <alexandre.leconte@insa-rennes.fr>
2023-11-13 13:52:31 +0100

    Correction de la taille de l'éditeur et de la load des fonts

--- src/main/java/fr/dwightstudio/jarmemu/gui/JArmEmuDialog.java
+++ src/main/java/fr/dwightstudio/jarmemu/gui/JArmEmuDialog.java
@@ -2,10 +2,7 @@ package fr.dwightstudio.jarmemu.gui;
 
 import atlantafx.base.layout.ModalBox;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.Control;
-import javafx.scene.layout.AnchorPane;
 
 public class JArmEmuDialog {
 
@@ -13,7 +10,6 @@ public class JArmEmuDialog {
 
     public JArmEmuDialog(Node graphic, String title, String content, boolean closable, Control ... controls) {
         box = new ModalBox();
-
 
 
         box.addContent(null);
