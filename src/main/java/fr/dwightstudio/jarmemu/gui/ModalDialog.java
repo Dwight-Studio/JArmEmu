c73d4ec687c09e85d8fb8e6ef49a8211e51b0525
Alexandre Leconte <alexandre.leconte@insa-rennes.fr>
2023-11-15 14:47:57 +0100

    Modification des ModalDialogs

--- src/main/java/fr/dwightstudio/jarmemu/gui/ModalDialog.java
+++ src/main/java/fr/dwightstudio/jarmemu/gui/ModalDialog.java
@@ -52,13 +52,13 @@ public class ModalDialog {
         box.setMaxSize(mainBox.getPrefWidth() + 40, mainBox.getPrefHeight() + 40);
     }
 
-    public ModalDialog(Node content, double h, double v) {
+    public ModalDialog(Node content, double width, double height) {
         AnchorPane.setTopAnchor(content, 20.0);
         AnchorPane.setRightAnchor(content, 20.0);
         AnchorPane.setLeftAnchor(content, 20.0);
         AnchorPane.setBottomAnchor(content, 20.0);
         box = new ModalBox(content);
-        box.setMaxSize(h + 40, v + 40);
+        box.setMaxSize(width + 40, height + 40);
     }
 
     public ModalBox getModalBox() {
