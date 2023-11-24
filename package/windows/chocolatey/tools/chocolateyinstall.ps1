af1f639d775ef319d6562426c6c2192e88b8f585
Kévin Tollemer <kevin.tollemer@insa-rennes.fr>
2023-11-24 09:23:05 +0100

    Chocolatey package added
    - Added scripts to build packaging for Chocolatey
    - Small update to the windows build script

---
+++ package/windows/chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/Dwight-Studio/JArmEmu/releases/download/v0.1.3/JArmEmu-0.1.3-BETA.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'JArmEmu*
+
+  checksum      = 'C1B65F358548C059E043ECD63CC96D8FFAFC10B616A7B0F5164A5A2D65533F77'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
