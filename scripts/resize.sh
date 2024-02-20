d357e56ec0e4672bfbd1c60105730d8c403adc1b
Alexandre Leconte <aleconte@dwightstudio.fr>
2024-02-20 10:37:55 +0100

    Mise à jour des icons

---
+++ scripts/resize.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#
+#            ____           _       __    __     _____ __            ___
+#           / __ \_      __(_)___ _/ /_  / /_   / ___// /___  ______/ (_)___
+#          / / / / | /| / / / __ `/ __ \/ __/   \__ \/ __/ / / / __  / / __ \
+#         / /_/ /| |/ |/ / / /_/ / / / / /_    ___/ / /_/ /_/ / /_/ / / /_/ /
+#        /_____/ |__/|__/_/\__, /_/ /_/\__/   /____/\__/\__,_/\__,_/_/\____/
+#                         /____/
+#     Copyright (C) 2024 Dwight Studio
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+# Aller à la racine du dépôt
+cd $(git rev-parse --show-toplevel) || exit 1
+
+cd ./src/main/resources/fr/dwightstudio/jarmemu/medias || exit 1
+
+for x in 16 32 64 128 256 512
+do
+	magick logo.png -resize ${x}x${x} favicon@${x}.png
+done
+
+for x in 250 200 150 125 100
+do
+	width=$(( (x*1920)/300 ))
+	height=$(( (x*1200)/300 ))
+	if [[ $x -eq 100 ]]
+	then
+		magick splash@300pct.png -resize ${width}x${height} splash.png
+	else
+		magick splash@300pct.png -resize ${width}x${height} splash@${x}pct.png
+	fi
+done
