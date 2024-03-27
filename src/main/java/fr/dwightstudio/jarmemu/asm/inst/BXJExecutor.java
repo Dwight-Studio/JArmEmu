e6d20760ac8e09c808bb205c579baa1452015793
yinx <tollemerkevin@gmail.com>
2024-03-27 09:04:38 +0100

    Translations
    - Added translatable messages to two functions

--- src/main/java/fr/dwightstudio/jarmemu/asm/inst/BXJExecutor.java
+++ src/main/java/fr/dwightstudio/jarmemu/asm/inst/BXJExecutor.java
@@ -2,6 +2,7 @@ package fr.dwightstudio.jarmemu.asm.inst;
 
 import fr.dwightstudio.jarmemu.asm.DataMode;
 import fr.dwightstudio.jarmemu.asm.UpdateMode;
+import fr.dwightstudio.jarmemu.gui.JArmEmuApplication;
 import fr.dwightstudio.jarmemu.sim.exceptions.SyntaxASMException;
 import fr.dwightstudio.jarmemu.sim.obj.Register;
 import fr.dwightstudio.jarmemu.sim.obj.StateContainer;
@@ -10,6 +11,6 @@ public class BXJExecutor implements InstructionExecutor<Register, Object, Object
     //TODO: Replace String by a translatable Message
     @Override
     public void execute(StateContainer stateContainer, boolean forceExecution, boolean updateFlags, DataMode dataMode, UpdateMode updateMode, Register arg1, Object arg2, Object arg3, Object arg4) {
-        throw new SyntaxASMException("BXJ instruction is not implemented yet.");
+        throw new SyntaxASMException(JArmEmuApplication.formatMessage("%exception.implementation.bxj"));
     }
 }
--- src/main/java/fr/dwightstudio/jarmemu/asm/inst/SWPExecutor.java
+++ src/main/java/fr/dwightstudio/jarmemu/asm/inst/SWPExecutor.java
@@ -25,14 +25,14 @@ package fr.dwightstudio.jarmemu.asm.inst;
 
 import fr.dwightstudio.jarmemu.asm.DataMode;
 import fr.dwightstudio.jarmemu.asm.UpdateMode;
+import fr.dwightstudio.jarmemu.gui.JArmEmuApplication;
 import fr.dwightstudio.jarmemu.sim.exceptions.SyntaxASMException;
 import fr.dwightstudio.jarmemu.sim.obj.Register;
 import fr.dwightstudio.jarmemu.sim.obj.StateContainer;
 
 public class SWPExecutor implements InstructionExecutor<Register, Register, Integer, Object> {
-    //TODO: Replace String by a translatable Message
     @Override
     public void execute(StateContainer stateContainer, boolean forceExecution, boolean updateFlags, DataMode dataMode, UpdateMode updateMode, Register arg1, Register arg2, Integer arg3, Object arg4) {
-        throw new SyntaxASMException("SWP instruction is deprecated");
+        throw new SyntaxASMException(JArmEmuApplication.formatMessage("%exception.implementation.swp"));
     }
 }
